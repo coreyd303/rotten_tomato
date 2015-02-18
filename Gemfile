7465b67a6689ac66313c150a228a8a2416483ad4
Corey Davis <coreyd303@gmail.com>
2015-02-18 10:04:38 -0700

    adding basic bootstrap to homepage

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
-gem 'sqlite3'
+gem 'pg'
+gem 'bootstrap-sass'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -11,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
+  gem 'sqlite3'
   gem 'rspec-rails'
   gem 'pry'
   gem 'capybara'
--- spec/features/user_view_spec.rb
+++ spec/features/user_view_spec.rb
@@ -14,5 +14,33 @@ RSpec.describe 'the user view', type: :feature do
       expect(page).to have_link('Last Weekends Box Office Results')
       expect(page).to have_link('Future Box Office Rankings')
     end
+
+    it 'sees the lasts weeks movie results page' do 
+      expect(current_path).to eq('/')
+      click_link('Last Weekends Box Office Results')
+
+      expect(current_path).to eq(results_path)
+      expect(page).to have_link('MovieYak Home')
+      expect(page).to have_link('Future Box Office Rankings')
+
+      exect(page).to_not have_link('Last Weekends Box Office Results')
+
+      click_link('MovieYak Home')
+      expect(current_path).to eq('/')
+    end
+
+    it 'sees the future rankings page' do
+      expect(current_path).to eq('/')
+      click_link('Future Box Office Rankings')
+
+      expect(current_path).to eq(future_path)
+      expect(page).to have_link('MovieYak Home')
+      expect(page).to have_link('Last Weekends Box Office Results')
+
+      expect(page).to_not have_link('Future Box Office Rankings')
+
+      click_link('MovieYak Home')
+      expect(current_path).to eq('/')
+    end
   end
 end
