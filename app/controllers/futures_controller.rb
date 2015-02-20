fb8086ab332ada5c215efd6a4ea2ea4f9f978b1a
Corey Davis <coreyd303@gmail.com>
2015-02-19 19:35:47 -0700

    adding new solution for random ranking values, still not really all that random

--- app/controllers/futures_controller.rb
+++ app/controllers/futures_controller.rb
@@ -2,17 +2,12 @@ class FuturesController < ApplicationController
 
   def index
     @movies = ordered_movies
-    @ranks   = fake_rank
+    @one_week = @movies.shuffle
     @date   = DateTime.now
   end
 
 private
 
-  def fake_rank
-    nums  = [1,2,3]
-    ranks = nums.sample(3, random:1..3)
-    ranks
-  end
   def ordered_movies
     movies          = Movie.first(3)
     ordered_by_rank = movies.sort { |x,y| y.average_rank <=> x.average_rank }
