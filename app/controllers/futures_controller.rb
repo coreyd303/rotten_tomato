1257c35c3422fd54e29855797006175628fb789a
Corey Davis <coreyd303@gmail.com>
2015-02-19 19:29:42 -0700

    adding table of predictions to futures page, randomizing numbers is really really hard

--- app/controllers/futures_controller.rb
+++ app/controllers/futures_controller.rb
@@ -1,22 +1,21 @@
 class FuturesController < ApplicationController
 
   def index
-    @movies = ranked_movies
-    @prediction = rand(1..params[:num].to_i)
+    @movies = ordered_movies
+    @ranks   = fake_rank
+    @date   = DateTime.now
   end
 
 private
 
+  def fake_rank
+    nums  = [1,2,3]
+    ranks = nums.sample(3, random:1..3)
+    ranks
+  end
   def ordered_movies
-    movies = Movie.first(params[:num])
+    movies          = Movie.first(3)
     ordered_by_rank = movies.sort { |x,y| y.average_rank <=> x.average_rank }
     ordered_by_rank
   end
 end
-
-# pull above method into a helper to be used within the classes
-
-# order them within the array
-# use there current position as their current rank
-# create foe ranking #'s fitting within the range N
-  # N being the number of selected movies
